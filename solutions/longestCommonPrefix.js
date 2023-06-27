b4339802966914ddb25e7a4f44c2007d9657f7dd
Mohammad Jahan Bakhsh <mehmedjahanbakhsh@gmail.com>
2023-06-28 00:28:43 +0430

    solution: 14. Longest Common Prefix

---
+++ solutions/longestCommonPrefix.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (!strs[0] || strs.length == 1) return strs[0] || "";
+  let i = 0;
+  while (strs[0][i] && strs.every((w) => w[i] === strs[0][i])) i++;
+
+  return strs[0].substr(0, i);
+};
