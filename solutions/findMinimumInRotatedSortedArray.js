5fe7d4860ff057a88147baf89b16b63c368c0b1d
Mohammad Jahan Bakhsh <mehmedjahanbakhsh@gmail.com>
2023-07-28 00:05:32 +0430

    solution: 153. Find Minimum in Rotated Sorted Array

--- solutions/findMinimumInRotatedSortedArray.js
+++ solutions/findMinimumInRotatedSortedArray.js
@@ -4,24 +4,21 @@
  */
 var findMin = function (nums) {
   const checkMinimum = (i) =>
-    ((i > 0 && nums[i] < nums[i - 1]) ||
-      (i == 0 && nums[i] < nums[nums.length - 1])) &&
-    ((i < nums.length - 1 && nums[i] < nums[i + 1]) ||
-      (i === nums.length - 1 && nums[i] < nums[0]));
+    nums[i] < nums[i === 0 ? nums.length - 1 : i - 1] &&
+    nums[i] < nums[i === nums.length - 1 ? 0 : i + 1];
 
-  const getFirstThird = () => Math.max(Math.floor(nums.length / 3), 0);
+  const getFirstThird = () => Math.floor(nums.length / 3);
   const getSecondThird = () =>
     Math.min(Math.ceil((nums.length / 3) * 2), nums.length - 1);
 
   let i = getFirstThird();
   let j = getSecondThird();
   while (nums.length > 1) {
-    if (checkMinimum(i)) return nums[i];
-    if (checkMinimum(j)) return nums[j];
-
     if (nums[i] > nums[j]) {
+      if (checkMinimum(j)) return nums[j];
       nums = nums.slice(i + 1, j);
     } else {
+      if (checkMinimum(i)) return nums[i];
       nums = nums.slice(0, i).concat(nums.slice(j + 1));
     }
     i = getFirstThird();
